7550b22a807274d066f186134d1fe04082666333
NYCSwan <megan.swanby@gmail.com>
2017-02-11 15:30:45 -0500

    Deleted messages file

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/messages', function (req, res) {
-    res.send('Add a book')
-});
-
 module.exports = router;
